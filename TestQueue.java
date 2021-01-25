ab7213d6dfd1eeb95ecb5ec0fc0e58dfe59daec0
Xiaoyuer-6 <74359600+Xiaoyuer-6@users.noreply.github.com>
2021-01-25 22:25:25 +0800

    Update TestQueue.java

--- TestQueue.java
+++ TestQueue.java
@@ -1,12 +1,5 @@
 package queuedemo;
 
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: GaoBo
- * Date: 2021-01-24
- * Time: 9:58
- */
 public class TestQueue {
     public static void main(String[] args) {
         MyQueue myQueue = new MyQueue();
