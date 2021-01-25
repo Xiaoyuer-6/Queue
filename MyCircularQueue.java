9562d4443e6c7c0ad3fc9bd4eea8bef5bed297e9
Xiaoyuer-6 <74359600+Xiaoyuer-6@users.noreply.github.com>
2021-01-25 22:20:28 +0800

    Update MyCircularQueue.java

--- MyCircularQueue.java
+++ MyCircularQueue.java
@@ -1,12 +1,6 @@
 package queuedemo;
 
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: GaoBo
- * Date: 2021-01-24
- * Time: 10:47
- */
+
 public class MyCircularQueue {
 
     private int front;
